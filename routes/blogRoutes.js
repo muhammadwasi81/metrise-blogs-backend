eac3149b6c485c0a20b9e875b93cb840912b0e98
muhammadwasi81 <wasiarain819@gmail.com>
2024-11-07 21:38:06 +0500

    Add endpoint for slug

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -5,12 +5,14 @@ import {
   getBlogPostById,
   updateBlogPost,
   deleteBlogPost,
+  getBlogPostBySlug,
 } from "../controllers/blogController.js";
 import { upload } from "../config/cloudinaryConfig.js";
 const router = express.Router();
 
 router.post("/create-blog", upload.single("image"), createBlogs);
 router.get("/get-blogs", getAllBlog);
+router.get("/get-blog/:slug", getBlogPostBySlug);
 router.get("/get-blog/:id", getBlogPostById);
 router.put("/update-blog/:id", updateBlogPost);
 router.delete("/delete-blog/:id", deleteBlogPost);
